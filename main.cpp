2acdf06626daa108011e3d6b401cbe3d55c0c145
Adam Berdis <adam.berdis@hotmail.com>
2021-03-08 16:44:34 +0000

    CMakeLists and main.cpp update Added links to required libraries WiringPi and thread to CMakeLists. Copied the WiringPi setup for one ADC channel from the ADC-test into the main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
-// Insert main code for the Laser Harp
+#include <iostream>
+#include <thread>
+#include <wiringPi.h>
+#include <mcp3004.h>
+
+
+
+// SPI definitions
+#define SPI_CHAN 0
+#define MY_PIN 12345
+
+
+
+
+
+
+int main()
+{
+	int x;
+
+
+
+
+// SPI setup
+  	wiringPiSetup() ;
+  	mcp3004Setup (MY_PIN, SPI_CHAN); // 3004 and 3008 are the same 4/8 channels
+
+// Read the digitalvalue from the ADC pin
+	x = analogRead (MY_PIN);
+// Write the value on the screen
+	std::cout << "Digital signal:" << x << std::endl;
+
+
+
+
+
+
+return 0;
+}
