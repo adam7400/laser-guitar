da3e445f76630e75142be012f7e079628e8a8c38
Alex Croke <77842288+alexcroke@users.noreply.github.com>
2021-03-16 19:06:47 +0000

    Single String Test using .wav files
    Simple If statement is implemenented to monitor the output of the ADC digital signal which varies when the laser (string) is broken by the users finger

---
+++ Version 1 Single String/main.cpp
@@ -0,0 +1,40 @@
+#include <wiringPi.h>
+#include <mcp3004.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+// SPI setup
+#define SPI_CHAN 0
+#define MY_PIN 12345
+
+int main(void)
+{
+int x,i;	// Digital signal
+float v;	// Voltage (V)
+float k = 3.3/1024; //ADC resolution
+
+// SPI setup
+  wiringPiSetup() ;
+  mcp3004Setup (MY_PIN, SPI_CHAN); // 3004 and 3008 are the same 4/8 channels
+
+while(1){
+	x = analogRead (MY_PIN); 	// Read value from CH0 on ADC
+	
+	if (x < 500) { 
+	  cout<<"\nDigital signal: "<<x<<endl;  
+	  wget https://github.com/adam7400/space-harp/tree/main/Version%201%20Single%20String/Sound%20Libary%20(WAV%20files)/samples_a.wav
+	  aplay a.wav 
+	} else {
+	  cout<<"\nDigital signal: "<<x<<endl;
+	}
+	
+	cout<<"\nDigital signal: "<<x<<endl;	// Print the value of digital signal
+					
+	cout<<"Voltage:  "<<k*(float)(x)<<"V"<<endl; // Print voltage
+	
+	delay(200); 
+}
+	return 0;
+
