387c794fdf5b44b53473a2877945aed1cc33f080
TanielGoetcherian <taniel.goetcherian@gmail.com>
2021-03-17 13:23:02 +0000

    added cmakelists, renamed some things

---
+++ Version_1_Single_String/main.cpp
@@ -0,0 +1,49 @@
+#include <wiringPi.h>
+#include <mcp3004.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+// SPI setup
+#define SPI_CHAN 0
+#define MY_PIN 12345
+
+
+// *** Potentially have to call all required sound files at the start of main using wget so that all files are available when digital signal is interrupted***
+// Then use aplay when required further down the main.cpp (Sampling Rates using aplay ~r 48000 Hz a.wav will need to be looked at so audio can be played from audio jack)
+
+int main(void)
+{
+int x,i;	// Digital signal
+float v;	// Voltage (V)
+float k = 3.3/1024; //ADC resolution
+
+
+// SPI setup
+  wiringPiSetup() ;
+  mcp3004Setup (MY_PIN, SPI_CHAN); // 3004 and 3008 are the same 4/8 channels
+
+while(1){
+	x = analogRead (MY_PIN); 	// Read value from CH0 on ADC
+
+// if statement to monitor digital signal 	
+	if (x < 500) {  				// 500 value may change depending on final resistor selected, currently 30k resistor
+	  cout<<"\nDigital signal: "<<x<<endl;  
+	
+	// Use aplay function that is built into raspbain for audio
+	  wget https://github.com/adam7400/space-harp/tree/main/Version%201%20Single%20String/Sound%20Libary%20(WAV%20files)/samples_a.wav // wget collects file from GitHub 
+	  aplay a.wav // .WAV file is played using sampling rate of 8kHz *** THIS IS AN ISSUE ***
+
+	} else {
+	  cout<<"\nDigital signal: "<<x<<endl;
+	}
+	
+	cout<<"\nDigital signal: "<<x<<endl;	// Print the value of digital signal
+					
+	cout<<"Voltage:  "<<k*(float)(x)<<"V"<<endl; // Print voltage
+	
+	// Delay removed so that response of digital signal is essentially the response of the photoresistor
+}
+	return 0;
+
