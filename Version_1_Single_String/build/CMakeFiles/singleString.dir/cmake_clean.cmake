3b0c3a38ee9e1af5527e54745a6a38e814cb33ea
Adam Berdis <adam.berdis@hotmail.com>
2021-03-18 13:09:34 +0000

    Added a build file into the single string test

---
+++ Version_1_Single_String/build/CMakeFiles/singleString.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/singleString.dir/main.cpp.o"
+  "CMakeFiles/singleString.dir/main.cpp.o.d"
+  "singleString"
+  "singleString.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/singleString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
