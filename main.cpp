ad69c00a31775022e443620253f70b8126987e5d
Adam Berdis <adam.berdis@hotmail.com>
2021-04-01 11:24:15 +0100

    Added possible implementation of changing chords with user keyboard input - the tones do not correspond to the real desired sounds in this version, should be adjusted accordingly later

--- main.cpp
+++ main.cpp
@@ -23,12 +23,25 @@ void play(int DS, int threshold, char tone) {
 	strcpy(b,"aplay -d 1 samples_b.wav");  
 
 	if (DS < threshold)
-		if (tone == 'a'){
-	system(a);}
-		else if (tone == 'b'){
-	system(b);}
-
+		if (tone=='a'){ 
+		system(a);
+		}
+		else if (tone=='b'){ 
+		system(b);
+		}
 	}
+
+class Chord{
+public:
+
+char E;
+char B;
+char G;
+char D;
+char A;
+char e; 
+
+}; 
 	
 
 
@@ -38,7 +51,7 @@ int main (int argc, const char* argv[])
 {
 	int x1;	 		// input from ADC channel 1
 	int th1;		// digital signal threshold
-	char tone;		// tone to be played
+	int chord;
 	
 // Threshold value based on the resistor used
 th1 = 900;
@@ -48,8 +61,52 @@ th1 = 900;
   	wiringPiSetup() ;
   	mcp3004Setup (MY_PIN, SPI_CHAN); // 3004 and 3008 are the same 4/8 channels
 
-std::cout<<"Type in the tone to be played (OPTIONS: a,b): "<< std::endl;
-std::cin >> tone;
+
+// define chords
+Chord Ami;
+/* Ami.E = ;
+Ami.B = ;
+Ami.G = ;
+Ami.D = ;
+Ami.A = ; */
+Ami.e = 'a';  // should be different tone, this is just for testing
+
+Chord A;
+/* A.E = ;
+A.B = ;
+A.G = ;
+A.D = ;
+A.A = ; */
+A.e = 'b';
+
+// define the object for the current chord
+Chord current;
+
+
+
+std::cout<<"Pick a chord (Ami - 1, A - 2): "<<std::endl;
+std::cin>> chord;
+
+
+if (chord == 1) { 
+/* current.E = Ami.E;
+current.B = Ami.B;
+current.G = Ami.G;
+current.D = Ami.D;
+current.A = Ami.A; */
+current.e = Ami.e;
+} 
+
+else if (chord == 2) { 
+
+/* current.E = A.E;
+current.B = A.B;
+current.G = A.G;
+current.D = A.D;
+current.A = A.A; */
+current.e = A.e;
+} 
+
 
 
 while (1)
@@ -60,11 +117,15 @@ while (1)
 	
 
 
-// Create a thread representing string 1
-std::thread string1(play,x1,th1,tone);
+	// Create a thread representing string 1 (high E string)
+std::thread estring(play,x1,th1,current.e);
+	//Create a thread representing string 2 (A string)
+//std::thread Astring(play,x2,th2,current.A);
+
 
 // Wait until thread terminates
-string1.join();
+estring.join();
+//Astring.join();
 
 
 }
