b41070bf6059076ad7da6c140992e80853a126a0
Alex Croke <77842288+alexcroke@users.noreply.github.com>
2021-04-19 17:21:37 +0100

    Housekeeping

--- GPIO_test/ADC_test/main.cpp
+++ GPIO_test/ADC_test/main.cpp
@@ -9,32 +9,56 @@ using namespace std;
 #define SPI_CHAN 0
 #define MY_PIN 100
 
+
+
 int main(void)
 {
-int x0,x1,x2,x3,x4,x5;	// Digital signal
-float v;	// Voltage (V)
-float k = 3.3/1024; //ADC resolution
 
-// SPI setup
-  wiringPiSetup() ;
+	// Digital signal
+int x0,x1,x2,x3,x4,x5;	
+
+	// Voltage (V)
+float v;	
+
+	//ADC resolution
+float k = 3.3/1024; 
+
+	// SPI setup
+  wiringPiSetup();
   mcp3004Setup (MY_PIN, SPI_CHAN); // 3004 and 3008 are the same 4/8 channel
 
 while(1){
-	x0 = analogRead (MY_PIN); 	// Read value from CH0 on ADC
+
+		// Read the channel value on the ADC 
+
+
+	x0 = analogRead (MY_PIN); 	
 	x1 = analogRead (MY_PIN + 1);
 	x2 = analogRead (MY_PIN + 2);
 	x3 = analogRead (MY_PIN + 3);
 	x4 = analogRead (MY_PIN + 4);
 	x5 = analogRead (MY_PIN + 5);
-	cout<<"\nCH0: "<<x0<<endl;	// Print the value of digital signal
-	cout<<"CH1: "<<x1<<endl;	// Print the value of digital signal
+
+
+		// Print digital signal value that will range between 0 and 1023.
+
+
+	cout<<"\nCH0: "<<x0<<endl;	
+	cout<<"CH1: "<<x1<<endl;		
 	cout<<"CH2: "<<x2<<endl;	
-	cout<<"CH3: "<<x3<<endl;
+	cout<<"CH3: "<<x3<<endl;		
 	cout<<"CH4: "<<x4<<endl;	
 	cout<<"CH5: "<<x5<<endl;
-	//cout<<"Voltage:  "<<k*(float)(x)<<"V"<<endl; // Print voltage
 	
-	delay(100);
+
+		 // Print delay so ADC results can be understood by user
+	
+	delay(100); 
 }
 	return 0;
 }
+
+
+
+
+
